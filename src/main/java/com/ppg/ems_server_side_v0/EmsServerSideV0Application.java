538d2a17baf8944991a928d01ecc508fb1a2d017
achref <rehimiachref@gmail.com>
2025-03-13 01:05:25 +0100

    Initial commit

---
+++ src/main/java/com/ppg/ems_server_side_v0/EmsServerSideV0Application.java
@@ -0,0 +1,13 @@
+package com.ppg.ems_server_side_v0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsServerSideV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsServerSideV0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppg/ems_server_side_v0/EmsServerSideV0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppg.ems_server_side_v0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsServerSideV0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
